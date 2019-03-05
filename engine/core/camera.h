f03952e39cba8eb9e96ce19c0f3d3cc972a2657a
BagelGoose <ptrb0164@humbermail.ca>
2019-03-05 12:59:31 -0500

    Changes to camera and started coding parts of Camera
    GLM has it's own vector and matrix transformation code, so the math I imported was redundant. Feel free to remove it if no one finds it useful.

--- engine/core/camera.h
+++ engine/core/camera.h
@@ -1,9 +1,10 @@
 #ifndef CAMERA_H
 #define	CAMERA_H
 
-#include "math\MMath.h"
-
-using namespace MATH;
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+#include <gl\GL.h>
+#include <vector>
 
 /**
  * \class	camera
@@ -12,52 +13,157 @@ using namespace MATH;
  *
  * \state   Template with methods and variables ready to be filled and connected by [Component]
  *
- * \Also imported Vector and MMath code in case groups need to use their Vector or Matrix methods
+ * \
  *
  * \author	Brandon_P
  * \date	3/3/2019 ->> 3/4/2019
  */
 
-namespace component {//namespace subject to change
+enum Camera_Movement
+{
+	FORWARD,
+	BACKWARDS,
+	LEFT,
+	RIGHT
+};
 
-	class camera
+
+const GLfloat YAW = -90.0f;
+const GLfloat PITCH = 0.0f;
+const GLfloat SPEED = 8.0f;
+const GLfloat SENSITIVITY = 0.4f; //Mouse Sensitivity
+const GLfloat FOV = 45.0f; //Field of VIew
+
+class camera
+{
+private:
+	//Camera location Values
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec3 worldUP;
+
+	//Camera rotation values
+	glm::vec3 yaw;
+	glm::vec3 pitch;
+
+	//Camera movement values
+	glm::vec3 movementSpeed;
+	glm::vec3 mouseSensitivity;
+	glm::vec3 fov;
+
+	void updateCamera()
 	{
-	private:
-		// Type of camera Lens (Wide View or Focus View)
-		Matrix4 projectionMatrix;
-		Matrix4 viewMatrix;
+		glm::vec3 front;
 
-		//Camera Variables
-		Vec3 position;
-		Vec3 rotation;
-		Vec3 direction, target;
-		Vec3 front, up;
+		front.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+		front.y = sin(glm::radians(this->pitch));
+		front.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
 
+		this->front = glm::normalize(front);
+		this->right = glm::normalize(glm::cross( this->front, this->worldUP));
+		this->up = glm::normalize(glm::cross(this->right, this->front));
+	}
 
-	public:
-		camera(const int viewportWidth, const int viewportHeight, const Vec3 position_);
-		~camera();
+public:
 
-		float aspectRatio;
-		camera* currentCamera;
+	camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+		GLfloat yaw = YAW, GLfloat pitch = PITCH, ) : front(glm::vec3(0.0f, 0.0f, -1.0f)), movementSpeed(SPEED), mouseSensitivity(SENSITIVITY), fov(FOV))
+		{
 
-		//Matrix Methods
-		void UpdateViewMatrix();
-		Matrix4&  getProjectionMatrix();
-		Matrix4&  getViewMatrix();
+		this->position = position;
+		this->worldUP = up;
+		this->yaw = yaw;
+		this->pitch = pitch;
+		this->updateCamera();
+		}
 
-		//Set Camera Movement Components
-		void setPosition(const Vec3& position_);
-		void setRotation(const Vec3& rotation_);
-		void setUpDirection(const Vec3& dir_);
+	camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY, GLfloat upZ, GLfloat yaw, GLfloat pitch) :
+		front(glm::vec3(0.0f, 0.0f, -1.0f)), movementSpeed(SPEED), mouseSensitivity(SENSITIVITY), fov(FOV))
+	{
+	this->position = glm::vec3(posX, posY, posZ);
+	this->worldUP = glm::vec3(upX, upY, upZ);;
+	this->yaw = yaw;
+	this->pitch = pitch;
+	this->updateCamera();
+	}
 
-		/// Return the 4x4 rotational matrix
-		Matrix4 GetMatrix4() const { return viewMatrix; }
+	glm::mat4 GetViewMatrix()
+	{
+		return glm::lookAt(this->position, this->position + this->front, this->up);
+	}
 
-		/// Return the 3x3 rotational matrix, rotational matrix is a 3x3
-		Matrix3 GetMatrix3() const { return Matrix3(viewMatrix); }
+	void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime)
+	{
+		GLfloat velocity = this->movementSpeed * deltaTime;
 
-	};
+		if (FORWARD == direction)
+		{
+			this->position += this->front * velocity;
+		}
+		if (BACKWARDS == direction)
+		{
+			this->position -= this->front * velocity;
+		}
+		if (LEFT == direction)
+		{
+			this->position -= this->right * velocity;
+		}
+		if (RIGHT == direction)
+		{
+			this->position += this->right * velocity;
+		}
+
+	}
+
+	void ProcessMouseMovement(GLfloat xOffset, GLfloat yOffset, GLboolean constraintPitch = true)
+	{
+		xOffset *= this->mouseSensitivity;
+		yOffset *= this->mouseSensitivity;
+
+		this->yaw += xOffset;
+		this->pitch += yOffset;
+		//Prevent Screen from flipping 
+		if (constrainPitch)
+		{
+			if (this->pitch > 89.0f)
+			{
+				this->pitch = 89.0f;
+			}
+
+			if (this->pitch < -89.0f)
+			{
+				this->pitch = -89.0f;
+			}
+
+		}
+
+		this->updateCamera();
+	}
+
+	void ProccessMouseScroll(GLfloat yOffset) {
+		if(this->fov >= 1.0f && this->fov <=45.0f)
+		{
+			this->fov -= yOffset;
+		}
+
+		if (this->fov <= 1.0f)
+		{
+			this->fov = 1.0f;
+		}
+
+		if (this->fov >= 45.0f)
+		{
+			this->fov = 45.0f;
+		}
+
+	}
+
+	GLfloat GetZoom()
+	{
+		return this->fov;
+	}
 }
 
 
