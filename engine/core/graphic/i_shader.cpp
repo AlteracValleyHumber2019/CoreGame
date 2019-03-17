3c2399fea5c89686e85231dce0ee8f31bf6cf814
Jaymie Xu <jaymie.xsj@gmail.com>
2019-03-17 14:12:05 -0400

    Added shader

---
+++ engine/core/graphic/i_shader.cpp
@@ -0,0 +1,12 @@
+#include "pav_pch.h"
+#include "i_shader.h"
+
+size_t pav::IShader::GetChildrenSize() const
+{
+	return children_.size();
+}
+
+pav::IShader* pav::IShader::GetShaderAt(const size_t index)
+{
+
+}
---
+++ engine/core/graphic/shader_common.h
@@ -0,0 +1,21 @@
+#ifndef SHADER_COMMON_H
+#define SHADER_COMMON_H
+
+namespace pav
+{
+	/**
+	 * \enum	ShaderType
+	 *
+	 * \brief	Values that represent common shader types
+	 */
+	enum ShaderType
+	{
+		VERTEX_SHADER,
+		TESSELLATION_SHADER,
+		GEOMETRY_SHADER,
+		FRAGMENT_SHADER
+	};
+
+}
+
+#endif // SHADER_COMMON_H
