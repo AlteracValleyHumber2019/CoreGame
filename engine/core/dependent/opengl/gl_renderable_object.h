9161b32bacafadaeb611d6d8d0f026a48b4dc70b
Gabriel Kalil <gkalil23@gmail.com>
2019-03-17 17:54:46 -0400

    Update Renderable Object
    Update the OpenGL concrete renderable object class. created the data but waiting for the shader to proceed.

--- engine/core/dependent/opengl/gl_renderable_object.h
+++ engine/core/dependent/opengl/gl_renderable_object.h
@@ -2,6 +2,7 @@
 #define GL_RENDERABLE_OBJECT
 
 #include "core/graphics/i_renderable_object.h"
+#include "core/util/wrappers/type_wrappers.h"
 
 namespace pav {
 	/**
@@ -12,9 +13,24 @@ namespace pav {
 	 * \author	Gabriel Kalil
 	 * \date	3/17/2019
 	 */
+
+	struct GLRenderData
+	{
+		GAPIUInt vao;
+		GAPIUInt texture_vbo;
+		GAPIUInt instance_vbo = 0;
+		GAPIUInt ebo;
+		size_t indices_size;
+
+		GAPIUInt texture;
+	};
+
 	class GLRenderableObject : public IRenderableObject
 	{
+	protected:
+		GLRenderData gl_renderable_data_;
 
+		//TODO : Finish implementation of the class but waiting for the shader system.
 	};
 }
 #endif // !GL_RENDERABLE_OBJECT
