ab81bb8365506cb2ac8bfc41ec95ae6b27300a65
Landon Grant <landongrant@hotmail.com>
2019-04-02 16:27:18 -0400

    Minor Psuedocode GameState
    Added some plans for gamestate as comments

--- engine/core/GameState.cpp
+++ engine/core/GameState.cpp
@@ -10,3 +10,18 @@ pav::GameState::GameState()
 pav::GameState::~GameState()
 {
 }
+
+void pav::GameState::UpdateGameState()
+{
+	// Break the string into substrings and place inside of an array
+
+	// With the new array, store the first section of the substring into a temp string up to the ":" so "CurrentHealth"
+
+	// Compare the tempstring so that we know what datatype we need to parse the value to.
+
+	// if tempstring = "CurrentHealth", we need to parse to int
+
+	// Remove the text from the delimiter up to and including the :, so "CurrentHealth:", from the string
+
+	// We are left with just the value "100" so lets parse it to int
+}
--- engine/core/GameState.h
+++ engine/core/GameState.h
@@ -26,13 +26,15 @@ namespace pav
 
 		// The below lines are commented out to avoid errors. These classes will be added in the next sprint
 
-		// std::vector<tower> towers;			// Stores all towers in game
 		// std::vector<graveyard> graveyards;	// Stores all graveyards in game
 		// std::vector<unit> units;				// Stores all NPC and Enemy units/bosses in game (or maybe in section)
 
 	public:
 		GameState();
 		~GameState();
+
+		void UpdateGameState(); // This method will take in the JSON or String and will break the string into substrings based on delimiter and store into an array.
+		// With the newly created array we can iterate through each index and parse the value to the appropiate primative type.
 
 		// TODO: Discuss with the gameplay team, how do we want to handle getting information from the GameState
 		// That implementation will be here.
