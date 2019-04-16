f60821c384aab23e070a213615b5d76c012dec4f
Jaymie Xu <jaymie.xsj@gmail.com>
2019-04-16 02:33:09 -0400

    Changed toliet to white

--- assets/shaders/fragment_shader.glsl
+++ assets/shaders/fragment_shader.glsl
@@ -16,6 +16,6 @@ void main()
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * vec3(1, 1, 1);
 
-    vec3 result = (ambient + diffuse) * vec3(1, 1, 0);
+    vec3 result = (ambient + diffuse) * vec3(1, 1, 1);
     FragColor = vec4(result, 1.0);
 }
